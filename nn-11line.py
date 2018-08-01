4a173acaf2c24b9002f3f006a565b998a1e684f2
redman157 <32203726+redman157@users.noreply.github.com>
2018-08-01 09:17:46 +0700

    Create nn-11line.py

---
+++ nn-11line.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+learning_rate = 0,1
+epoch =  5000
+def sigmoid(x,deriv = False):
+  if(deriv == True):
+    return x*(1-x)
+  return 1/(1 + np.exp(-x))
+np.random.seed(1)
+X = np.array([ [0,0,1],
+               [0,1,1],
+               [1,0,1],
+               [1,1,1] ])
+y = np.array([[0,0,1,1]]).T
+weight = 2*np.random.random((3,1))-1
+
+for i in range(epoch):
+    layer_0 = X
+    layer_1 = sigmoid(np.dot(layer_0,weight))
+    layer_error = (y - layer_1)
+    delta = layer_error * sigmoid(layer_1,True)
+    weight += np.dot(layer_0.T,delta)
+print("ket qua sau training",layer_1)
+    
+
+  
